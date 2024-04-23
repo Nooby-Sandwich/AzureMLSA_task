94f773828acfaaead500d04c06402b35f1d7685a
Nooby Sandwich <avin.workspace@gmail.com>
2024-04-23 10:11:46 +0530

    Static Portfolio Website on Azure

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,19 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Avin Mantri",
+  title: "Computer Science Major",
+  email: "avin.workspace@gmail.com",
+  gitHub: "https://github.com/Nooby-Sandwich",
+  instagram: "https://www.instagram.com/mantriavin/",
+  linkedIn: "https://www.linkedin.com/in/avin-mantri/",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const secondaryColor = "#6F5";
 
 const App = () => {
   return (
